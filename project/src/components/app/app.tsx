08f7e29d31fcf419efdd521461682a3536713ef3
mashkust <mashkusti@gmail.com>
2022-02-27 15:59:10 +0300

    разные страницы

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,11 @@
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
+import {AppRoute} from '../../const';
 import MainCard from '../main-card';
+import SignIn from '../sign-in';
+import Player from '../player';
+import MoviePages from '../movie-page';
+import AddCard from '../add-review';
+import NotFoundPage from '../404-page';
 
 type Film = {
   name: string,
@@ -10,13 +17,53 @@ type Film = {
   id: number,
 }
 
+const PROMO_FILM: Film =
+{
+  name: 'The Grand Budapest Hotel',
+  poster: 'img/the-grand-budapest-hotel-poster.jpg',
+  preview: 'https://9.react.pages.academy/static/film/preview/revenant.jpg',
+  backgroundImage: 'img/bg-the-grand-budapest-hotel.jpg',
+  genre: 'Drama',
+  released: 2014,
+  id: 1,
+};
+
 type AppScreenProps = {
   promoFilm: Film;
   films: Film[];
 }
 
 function App({promoFilm, films}: AppScreenProps): JSX.Element {
-  return <MainCard promoFilm={promoFilm} films={films} />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element={<MainCard promoFilm={promoFilm} films={films}/>}
+        />
+        <Route
+          path={AppRoute.SignIn}
+          element={<SignIn/>}
+        />
+        <Route
+          path={AppRoute.Film}
+          element={<MoviePages film={PROMO_FILM} similarFilms={films}/>}
+        />
+        <Route
+          path={AppRoute.Player}
+          element={<Player/>}
+        />
+        <Route
+          path={AppRoute.AddReview}
+          element={<AddCard film={PROMO_FILM}/>}
+        />\
+        <Route
+          path="*"
+          element={<NotFoundPage/>}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
